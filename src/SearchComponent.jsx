f3c083d005902468f25f1b11a37f41138a338b5e
JennyWeij <jennyweij@outlook.com>
2023-11-27 17:36:52 +0100

    lagt till sökkomponent

---
+++ src/SearchComponent.jsx
@@ -0,0 +1,28 @@
+import React, { useState } from 'react';
+
+const SearchComponent = ({ onSearch }) => {
+  const [query, setQuery] = useState('');
+
+  const handleInputChange = (event) => {
+    setQuery(event.target.value);
+  };
+
+  const handleSearch = () => {
+    onSearch(query);
+  };
+
+  return (
+    <div>
+       <h1>Dictionary</h1>
+      <input
+        type="text"
+        placeholder="Search..."
+        value={query}
+        onChange={handleInputChange}
+      />
+      <button onClick={handleSearch}>Search</button>
+    </div>
+  );
+};
+
+export default SearchComponent;
