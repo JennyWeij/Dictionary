f437a4ec7b420fbc611b5d5faa3fc5eeb38f4d6e
JennyWeij <jennyweij@outlook.com>
2023-11-28 20:02:37 +0100

    lagt till så att användaren kan se ett felmeddelande om inget skrivs i inputfältet

--- src/SearchComponent.jsx
+++ src/SearchComponent.jsx
@@ -1,22 +1,30 @@
 import React, { useState } from 'react';
 import TextBox from './TextBox';
 
-function SearchComponent ({ onSearch }) {
+function SearchComponent({ onSearch }) {
   const [searchTerm, setSearchTerm] = useState('');
   const [isLoading, setIsLoading] = useState(false);
-  const [searchResult, setSearchResult] = useState(null); 
+  const [searchResult, setSearchResult] = useState(null);
+  const [errorMessage, setErrorMessage] = useState(null); // Add error message state
 
   const handleSearch = async (word) => {
+    if (!word.trim()) {
+      setErrorMessage('Please enter a word');
+      return;
+    }
+
+    setErrorMessage(null); // Reset error message
     setIsLoading(true);
+
     try {
-      const response = await fetch(`https://api.dictionaryapi.dev/api/v2/entries/en/${word}`
-      );
+      const response = await fetch(`https://api.dictionaryapi.dev/api/v2/entries/en/${word}`);
       const data = await response.json();
       setSearchResult(data);
     } catch (error) {
       console.error('Error fetching data:', error);
       setSearchResult(null);
-    } 
+    }
+
     setIsLoading(false);
   };
 
@@ -29,19 +37,20 @@ function SearchComponent ({ onSearch }) {
         placeholder="Enter word..."
         value={searchTerm}
         onChange={(e) => setSearchTerm(e.target.value)}
-        style={{ height: '35px', color: "beige", marginBottom:"20px", marginTop:"10px" }}
+        style={{ height: '35px', color: 'beige', marginBottom: '20px', marginTop: '10px' }}
       />
-      <br/>
-      <button style={{ marginBottom:"30px" }} onClick={() => handleSearch(searchTerm)}>
+      <br />
+      <button style={{ marginBottom: '30px' }} onClick={() => handleSearch(searchTerm)}>
         Search
       </button>
-      <br/><br/>
-  
-           {/* Pass searchResult to TextBox */}
-           <TextBox searchResult={searchResult} />
-    </div>
+      <br />
+      {/* Display error message if it exists */}
+      {errorMessage && <p style={{ color: 'red' }}>{errorMessage}</p>}
 
+      {/* Pass searchResult to TextBox */}
+      <TextBox searchResult={searchResult} />
+    </div>
   );
 }
 
-export default SearchComponent
+export default SearchComponent;
