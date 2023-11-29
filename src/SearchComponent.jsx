d3e7a217bd463e8dcb5241f996837f8b658bbe41
JennyWeij <jennyweij@outlook.com>
2023-11-29 13:35:05 +0100

    snyggat till koden

--- src/SearchComponent.jsx
+++ src/SearchComponent.jsx
@@ -12,10 +12,9 @@ function SearchComponent({ onSearch }) {
       setErrorMessage('Please enter a word');
       return;
     }
-
-    setErrorMessage(null); // Reset error message
+    setErrorMessage(null); 
     setIsLoading(true);
-
+    
     try {
       const response = await fetch(`https://api.dictionaryapi.dev/api/v2/entries/en/${word}`);
       const data = await response.json();
@@ -24,7 +23,6 @@ function SearchComponent({ onSearch }) {
       console.error('Error fetching data:', error);
       setSearchResult(null);
     }
-
     setIsLoading(false);
   };
 
